8c0b0329d15ee410eb6187befa3293be553d5eb3
chandana M <chandu11288@gmail.com>
2018-01-23 03:18:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,11 +13,9 @@ def won?(board)
 WIN_COMBINATIONS.each do |combination|
   if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X" ||
     board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
-    winning_combination = [];
-    winning_combination.push(combination)
     return combination
   else
-    false
+    return false
   end
  end
 end
