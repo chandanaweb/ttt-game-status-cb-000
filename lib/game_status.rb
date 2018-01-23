4b9c6534f04a8e06490eaed47243c7737823ebfa
chandana M <chandu11288@gmail.com>
2018-01-23 02:32:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,6 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+  board = ["X", "X", "X", "O", "O", " ", " ", " ", " "]
 
 def won(board)
 [0..8].each do |index|
