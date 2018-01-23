f1ee8c41e9254171898dc6f337fcbb4246c284af
chandana M <chandu11288@gmail.com>
2018-01-23 03:44:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,10 +47,16 @@ WIN_COMBINATIONS.each do |combination|
 end
 
 def over?(board)
-  incomplete_board = !full?(board)
-  if incomplete_board == true
-    return false
-  else
-    return true
+  WIN_COMBINATIONS.each do |combination|
+    if combination1 = board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X" ||
+       combination2 = board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+      return true
+    end
   end
-  end
+    draw = full?(board)
+    if draw == true
+      return true
+    else
+      return false
+   end
+   end
