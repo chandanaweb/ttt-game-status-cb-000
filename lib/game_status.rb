fd76bb966333cb3623fe3014bc33cef6e45edfbd
chandana M <chandu11288@gmail.com>
2018-01-23 03:03:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,11 +11,8 @@ def won?(board)
     return false
   end
 WIN_COMBINATIONS.each do |combination|
-  combination1 = board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
-  combination2 = board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
-  if (combination1 && combination2)
-    return false
-  elsif(combination1 || combination2)
+  if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X" ||
+    board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
     return combination
   else
     false
