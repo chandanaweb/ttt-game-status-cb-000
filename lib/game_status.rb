720a5af60449199056fcf6bf1ae94a2fbd04914b
chandana M <chandu11288@gmail.com>
2018-01-23 02:26:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,3 +5,13 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+
+def won(board)
+[0..8].each do |index|
+  if board[index] == 'X'
+     combinations = board[index]
+   end
+ end
+ WIN_COMBINATIONS.each do |combination|
+   combinations.include?(combination)
+end
