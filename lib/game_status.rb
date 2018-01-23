61b4bd9df512c0a2a726d7d9363e5ccf033cd4c2
chandana M <chandu11288@gmail.com>
2018-01-23 02:33:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,7 +10,8 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4
 def won(board)
 [0..8].each do |index|
   if board[index] == 'X'
-     combinations = board[index]
+     combinations = []
+     combinations.push(index)
    end
  end
  WIN_COMBINATIONS.each do |combination|
