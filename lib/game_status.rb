d6f9889cb6d17e4138a78d9965dd45a9d787703f
chandana M <chandu11288@gmail.com>
2018-01-23 02:43:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,23 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
   board = ["X", "X", "X", "O", "O", " ", " ", " ", " "]
 
-def won(board)
-[0..8].each do |index|
-  if board[index] == 'X'
-     combinations = []
-     combinations.push(index)
-   end
- end
- WIN_COMBINATIONS.each do |combination|
-   isAvailable = combinations.include?(combination)
-   if isAvailable == true
-    return combination
-   end
-end
-if isAvailable == false
-  return false
-end
-end
+
 
 def won(board)
 WIN_COMBINATIONS.each do |combination|
